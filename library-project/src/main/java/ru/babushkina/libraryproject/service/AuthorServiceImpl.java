2d3032829c801b64a50abd1c0076befe12a67244
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 15:39:15 +0300

    feature:18.10.2023 15:39 method getAuthorById was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorServiceImpl.java
@@ -2,10 +2,18 @@ package ru.babushkina.libraryproject.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.babushkina.libraryproject.dto.AuthorDto;
+import ru.babushkina.libraryproject.model.Author;
 import ru.babushkina.libraryproject.repository.AuthorRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService{
     private final AuthorRepository authorRepository;
+
+    @Override
+    public AuthorDto getAuthorById(Long id) {
+        Author author = authorRepository.findById(id).orElseThrow();
+        return convertToDto(author);
+    }
 }
