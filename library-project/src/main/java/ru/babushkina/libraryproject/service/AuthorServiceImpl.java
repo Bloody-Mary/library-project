f8cb479f91e105612aeaa39acff5509fd50ee8df
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 15:45:09 +0300

    feature:18.10.2023 15:44 method convertToDto was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorServiceImpl.java
@@ -3,8 +3,11 @@ package ru.babushkina.libraryproject.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.babushkina.libraryproject.dto.AuthorDto;
+import ru.babushkina.libraryproject.dto.BookDto;
 import ru.babushkina.libraryproject.model.Author;
 import ru.babushkina.libraryproject.repository.AuthorRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,22 @@ public class AuthorServiceImpl implements AuthorService{
     public AuthorDto getAuthorById(Long id) {
         Author author = authorRepository.findById(id).orElseThrow();
         return convertToDto(author);
+    }
+
+    private AuthorDto convertToDto(Author author) {
+        List<BookDto> bookDtoList = author.getBooks()
+                .stream()
+                .map(book -> BookDto.builder()
+                        .genre(book.getGenre().getName())
+                        .name(book.getName())
+                        .id(book.getId())
+                        .build()
+                ).toList();
+        return AuthorDto.builder()
+                .books(bookDtoList)
+                .id(author.getId())
+                .name(author.getName())
+                .surname(author.getSurname())
+                .build();
     }
 }
