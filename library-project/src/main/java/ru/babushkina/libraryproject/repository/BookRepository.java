e5d03132525a693893ccd699ced85814c8cb71b7
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 15:18:35 +0300

    fix:18.10.2023 15:18 <Book, Long> was added to interface

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package ru.babushkina.libraryproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.babushkina.libraryproject.model.Book;
 
-public interface BookRepository extends JpaRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
