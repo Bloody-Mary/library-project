7206b029aaf0f6d7ab2690133bd0dc29ed27ed82
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 16:14:29 +0300

    feature:18.10.2023 16:14 variables bookRepository and authorRepository were added.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/GenreServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/GenreServiceImpl.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.babushkina.libraryproject.dto.GenreDto;
 import ru.babushkina.libraryproject.model.Genre;
+import ru.babushkina.libraryproject.repository.AuthorRepository;
+import ru.babushkina.libraryproject.repository.BookRepository;
 import ru.babushkina.libraryproject.repository.GenreRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GenreServiceImpl implements GenreService {
     private final GenreRepository genreRepository;
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
 
     @Override
     public GenreDto getGenreById(Long id) {
