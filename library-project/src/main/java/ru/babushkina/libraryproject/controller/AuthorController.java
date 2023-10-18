a5c32e84cfecc2dbe0d73d130ef3d39aef2f06fe
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 15:49:10 +0300

    feature:18.10.2023 15:49 final variable authorService was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/AuthorController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/AuthorController.java
@@ -1,4 +1,14 @@
 package ru.babushkina.libraryproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.babushkina.libraryproject.dto.AuthorDto;
+import ru.babushkina.libraryproject.service.AuthorService;
+
+@RestController
+@RequiredArgsConstructor
 public class AuthorController {
+    private final AuthorService authorService;
 }
