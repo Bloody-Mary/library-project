0f688e530d4102a925bbb343a4bd19b8cc97d271
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 13:50:14 +0300

    feature:18.10.2023 13:50 variables id and name were created

--- library-project/src/main/java/ru/babushkina/libraryproject/model/Book.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/Book.java
@@ -1,4 +1,21 @@
 package ru.babushkina.libraryproject.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
