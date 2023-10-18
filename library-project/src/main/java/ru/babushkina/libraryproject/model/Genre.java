361c76a3ba9f093e40c6198eea3b7b4ba6ca76e2
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 14:04:11 +0300

    feature:18.10.2023 14:04 variable id was created with annotations

--- library-project/src/main/java/ru/babushkina/libraryproject/model/Genre.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/Genre.java
@@ -1,4 +1,21 @@
 package ru.babushkina.libraryproject.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
