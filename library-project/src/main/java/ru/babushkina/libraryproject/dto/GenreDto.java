82cccb5522eccd7d94c7c2c27274647d0e831ad2
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 15:56:40 +0300

    feature:18.10.2023 15:56 private variables id and name were created.

--- library-project/src/main/java/ru/babushkina/libraryproject/dto/GenreDto.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/dto/GenreDto.java
@@ -1,4 +1,15 @@
 package ru.babushkina.libraryproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class GenreDto {
+    private Long id;
+    private String name;
 }
