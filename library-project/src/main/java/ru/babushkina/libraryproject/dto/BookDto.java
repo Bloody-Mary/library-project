e18405b7411e9f361fb69b45b122a109056d6868
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 15:26:16 +0300

    feature:18.10.2023 15:26 annotations were added to class

--- library-project/src/main/java/ru/babushkina/libraryproject/dto/BookDto.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/dto/BookDto.java
@@ -1,4 +1,14 @@
 package ru.babushkina.libraryproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class BookDto {
+
 }
