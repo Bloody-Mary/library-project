d187089581b9162af7d8c2cd638d33657f09b610
Bloody-Mary <marianelalieben@gmail.com>
2023-10-18 16:02:23 +0300

    feature:18.10.2023 16:02 method getGenreById was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/GenreServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/GenreServiceImpl.java
@@ -2,10 +2,18 @@ package ru.babushkina.libraryproject.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.babushkina.libraryproject.dto.GenreDto;
+import ru.babushkina.libraryproject.model.Genre;
 import ru.babushkina.libraryproject.repository.GenreRepository;
 
 @Service
 @RequiredArgsConstructor
 public class GenreServiceImpl implements GenreService {
     private final GenreRepository genreRepository;
+
+    @Override
+    public GenreDto getGenreById(Long id) {
+        Genre genre = genreRepository.findById(id).orElseThrow();
+        return convertToDto(genre);
+    }
 }
