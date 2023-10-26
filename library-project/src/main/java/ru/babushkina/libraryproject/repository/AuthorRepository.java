9fecf206b87568c063bb93635addadcb2b4fc934
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 20:06:05 +0300

    feature:26.10.2023 20:05 method findAuthorByNameBySQL was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/AuthorRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/AuthorRepository.java
@@ -1,10 +1,14 @@
 package ru.babushkina.libraryproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.babushkina.libraryproject.model.Author;
 
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     Optional<Author> findAuthorByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM author WHERE name = ?")
+    Optional<Author> findAuthorByNameBySQL(String name);
 }
