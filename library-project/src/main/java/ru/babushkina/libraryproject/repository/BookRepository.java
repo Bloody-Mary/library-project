cb20e9a93a1534a44e10ff2538e56aa589493c9b
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 20:20:37 +0300

    feature:26.10.2023 20:20 new interface JpaSpecificationExecutor was added to interface BookRepository.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
@@ -1,14 +1,15 @@
 package ru.babushkina.libraryproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import ru.babushkina.libraryproject.model.Book;
 
 import java.util.Optional;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     Optional<Book> findBookByName(String name);
+
     @Query(nativeQuery = true, value = "SELECT * FROM BOOK WHERE name = ?")
     Optional<Book> findBookByNameBySQL(String name);
-
 }
