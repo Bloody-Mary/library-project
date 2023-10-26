2f9f33b57d835d3ca537de4707986536d2ff4428
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 17:02:24 +0300

    feature:26.10.2023 17:02 variable List<Genre> genre was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/dto/AuthorDto.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/dto/AuthorDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.babushkina.libraryproject.model.Genre;
 
 import java.util.List;
 
@@ -18,4 +19,5 @@ public class AuthorDto {
     private String name;
     private String surname;
     private List<BookDto> books;
+    private List<Genre> genre;
 }
