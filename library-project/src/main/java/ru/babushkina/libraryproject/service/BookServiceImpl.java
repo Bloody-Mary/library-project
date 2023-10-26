0cc643d8f40b3607564317a96a541c0f3a448fde
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 16:34:24 +0300

    feature:26.10.2023 16:34 annotations @Service and @RequiredArgsConstructor were added to class and constructor was deleted.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
@@ -1,15 +1,15 @@
 package ru.babushkina.libraryproject.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.babushkina.libraryproject.dto.BookDto;
 import ru.babushkina.libraryproject.model.Book;
 import ru.babushkina.libraryproject.repository.BookRepository;
 
+@Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService{
     private final BookRepository bookRepository;
-
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public BookDto getByNameV1(String name) {
