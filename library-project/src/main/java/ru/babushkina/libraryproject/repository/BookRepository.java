371e075b923bcd4a2f7287f82f2d116742e6fd02
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 15:57:08 +0300

    feature:26.10.2023 16:57 method findBookByName was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
@@ -3,5 +3,8 @@ package ru.babushkina.libraryproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.babushkina.libraryproject.model.Book;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findBookByName(String name);
 }
