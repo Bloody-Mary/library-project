3ce52e9c74cb0e2b6bf725e015145581aca05d21
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 15:59:00 +0300

    feature:26.10.2023 16:58 method findAuthorByName was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/AuthorRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/AuthorRepository.java
@@ -3,5 +3,8 @@ package ru.babushkina.libraryproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.babushkina.libraryproject.model.Author;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findAuthorByName(String name);
 }
