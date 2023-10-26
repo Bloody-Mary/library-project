434d7e46380fe01f7d47f3bcf0d5ccaa11622b35
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 16:08:01 +0300

    feature:26.10.2023 16:07 method getByNameV1 was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
@@ -1,7 +1,14 @@
 package ru.babushkina.libraryproject.service;
 
+import ru.babushkina.libraryproject.dto.BookDto;
+import ru.babushkina.libraryproject.model.Book;
 import ru.babushkina.libraryproject.repository.BookRepository;
 
 public class BookServiceImpl implements BookService{
     private final BookRepository bookRepository;
+    @Override
+    public BookDto getByNameV1(String name) {
+       Book book = bookRepository.findBookByName(name).orElseThrow();
+       return convertEntityToDto(book);
+    }
 }
