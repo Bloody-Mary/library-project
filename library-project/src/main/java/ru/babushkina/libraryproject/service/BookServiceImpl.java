1628115dc3f2a80c863dca26fd0f57b26cf73a3c
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 16:10:28 +0300

    feature:26.10.2023 16:10 bookRepository constructor was added and method convertEntityToDto was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
@@ -6,9 +6,22 @@ import ru.babushkina.libraryproject.repository.BookRepository;
 
 public class BookServiceImpl implements BookService{
     private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public BookDto getByNameV1(String name) {
        Book book = bookRepository.findBookByName(name).orElseThrow();
        return convertEntityToDto(book);
+    }
+
+    private BookDto convertEntityToDto(Book book) {
+        return BookDto.builder()
+                .id(book.getId())
+                .genre(book.getGenre().getName())
+                .name(book.getName())
+                .build();
     }
 }
