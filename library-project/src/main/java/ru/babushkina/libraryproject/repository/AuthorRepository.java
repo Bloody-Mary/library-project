eb0e575cfa280e65738abdc3d15b069646f92487
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 20:29:39 +0300

    feature:26.10.2023 20:29 new interface JpaSpecificationExecutor was added to interface AuthorRepository.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/AuthorRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/AuthorRepository.java
@@ -1,12 +1,13 @@
 package ru.babushkina.libraryproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import ru.babushkina.libraryproject.model.Author;
 
 import java.util.Optional;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long>, JpaSpecificationExecutor<Author> {
     Optional<Author> findAuthorByName(String name);
 
     @Query(nativeQuery = true, value = "SELECT * FROM author WHERE name = ?")
