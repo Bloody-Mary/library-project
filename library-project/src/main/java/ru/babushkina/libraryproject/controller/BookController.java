57f33d8da70e2fc59242d15c91047329d2effda4
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 16:28:29 +0300

    feature:26.10.2023 16:28 method getBookByName was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/BookController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/BookController.java
@@ -10,4 +10,9 @@ import ru.babushkina.libraryproject.service.BookService;
 @RestController
 @RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+    @GetMapping("/book")
+    BookDto getBookByName(@RequestParam("name") String name) {
+        return bookService.getByNameV1(name);
+    }
 }
