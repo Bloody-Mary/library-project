36d00ec02a3cef7b57585440087157442d896d44
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 19:44:37 +0300

    feature:26.10.2023 19:44 method findBookByNameBySQL was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/BookRepository.java
@@ -1,10 +1,14 @@
 package ru.babushkina.libraryproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.babushkina.libraryproject.model.Book;
 
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findBookByName(String name);
+    @Query(nativeQuery = true, value = "SELECT * FROM BOOK WHERE name = ?")
+    Optional<Book> findBookByNameBySQL(String name);
+
 }
