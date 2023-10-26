7af00536be05c174280aa03c4e587ffd94620f88
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 16:05:16 +0300

    feature:26.10.2023 16:05 variable bookRepository was created.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
@@ -1,4 +1,7 @@
 package ru.babushkina.libraryproject.service;
 
+import ru.babushkina.libraryproject.repository.BookRepository;
+
 public class BookServiceImpl implements BookService{
+    private final BookRepository bookRepository;
 }
