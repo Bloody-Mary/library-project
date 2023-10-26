e13b078ba1525c09f21154cac37a415f88a9abfc
Bloody-Mary <marianelalieben@gmail.com>
2023-10-26 16:25:19 +0300

    feature:26.10.2023 16:25 class BookController was created.

---
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/BookController.java
@@ -0,0 +1,13 @@
+package ru.babushkina.libraryproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.babushkina.libraryproject.dto.BookDto;
+import ru.babushkina.libraryproject.service.BookService;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+}
