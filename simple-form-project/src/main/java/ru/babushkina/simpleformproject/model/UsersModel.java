12877a49e9cc257fd7b2927745bd044c8157e288
Bloody-Mary <marianelalieben@gmail.com>
2023-10-27 12:17:18 +0300

    feature:27.10.2023 12:17 methods equals and hashCode were redefined.

--- simple-form-project/src/main/java/ru/babushkina/simpleformproject/model/UsersModel.java
+++ simple-form-project/src/main/java/ru/babushkina/simpleformproject/model/UsersModel.java
@@ -46,4 +46,17 @@ public class UsersModel {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersModel that = (UsersModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(login, that.login) && Objects.equals(password, that.password) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password, email);
+    }
 }
