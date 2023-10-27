b0cc0ec2cae8c0c504e308524306824426f398f0
Bloody-Mary <marianelalieben@gmail.com>
2023-10-27 12:01:54 +0300

    feature:27.10.2023 12:01 class UsersModel was created with variables and constructors.

---
+++ simple-form-project/src/main/java/ru/babushkina/simpleformproject/model/UsersModel.java
@@ -0,0 +1,49 @@
+package ru.babushkina.simpleformproject.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users_table")
+public class UsersModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String login;
+    String password;
+    String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
