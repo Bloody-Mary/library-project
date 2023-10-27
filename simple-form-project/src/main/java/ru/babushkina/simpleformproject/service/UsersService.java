ca5b70604e23d753f6f562ac1dbf13899b4e829a
Bloody-Mary <marianelalieben@gmail.com>
2023-10-27 12:43:29 +0300

    feature:27.10.2023 12:43 @Autowired annotation and variable UsersRepository were added.

--- simple-form-project/src/main/java/ru/babushkina/simpleformproject/service/UsersService.java
+++ simple-form-project/src/main/java/ru/babushkina/simpleformproject/service/UsersService.java
@@ -1,17 +1,20 @@
 package ru.babushkina.simpleformproject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.babushkina.simpleformproject.model.UsersModel;
+import ru.babushkina.simpleformproject.repository.UsersRepository;
 
 @Service
 public class UsersService {
+    @Autowired
+    private UsersRepository usersRepository;
     public UsersModel registerUser(String login, String password, String email) {
         if (login != null && password != null) {
             UsersModel usersModel = new UsersModel();
             usersModel.setLogin(login);
             usersModel.setPassword(password);
             usersModel.setEmail(email);
-
         }
     }
 }
