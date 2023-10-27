ad342b45b9eae00824e58431bcebe30759a90ce8
Bloody-Mary <marianelalieben@gmail.com>
2023-10-27 12:34:49 +0300

    feature:27.10.2023 12:34 method registerUser was created.

--- simple-form-project/src/main/java/ru/babushkina/simpleformproject/service/UsersService.java
+++ simple-form-project/src/main/java/ru/babushkina/simpleformproject/service/UsersService.java
@@ -1,4 +1,17 @@
 package ru.babushkina.simpleformproject.service;
 
+import org.springframework.stereotype.Service;
+import ru.babushkina.simpleformproject.model.UsersModel;
+
+@Service
 public class UsersService {
+    public UsersModel registerUser(String login, String password, String email) {
+        if (login != null && password != null) {
+            UsersModel usersModel = new UsersModel();
+            usersModel.setLogin(login);
+            usersModel.setPassword(password);
+            usersModel.setEmail(email);
+
+        }
+    }
 }
