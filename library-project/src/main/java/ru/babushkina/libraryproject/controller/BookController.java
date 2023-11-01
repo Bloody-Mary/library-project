0e554c2206e8c9f4940a747b89b072af6a364ecf
Bloody-Mary <marianelalieben@gmail.com>
2023-11-01 19:42:48 +0300

    feature:01.11.2023 19:42 POST method createBook was created in class BookController.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/BookController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/BookController.java
@@ -2,8 +2,10 @@ package ru.babushkina.libraryproject.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.babushkina.libraryproject.dto.BookCreateDto;
 import ru.babushkina.libraryproject.dto.BookDto;
 import ru.babushkina.libraryproject.service.BookService;
 
@@ -24,5 +26,10 @@ public class BookController {
     @GetMapping("/book/v3")
     BookDto getBookByNameV3(@RequestParam("name") String name) {
         return bookService.getByNameV3(name);
+    }
+
+    @PostMapping("/book/create")
+    BookDto createBook(@RequestParam BookCreateDto bookCreateDto) {
+        return bookService.createBook(bookCreateDto);
     }
 }
