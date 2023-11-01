866b09f6f4f79f9218aff659cbd242c06daf6db0
Bloody-Mary <marianelalieben@gmail.com>
2023-11-01 20:00:28 +0300

    feature:01.11.2023 20:00 method createAuthor was created in interface AuthorService.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorService.java
@@ -1,5 +1,6 @@
 package ru.babushkina.libraryproject.service;
 
+import ru.babushkina.libraryproject.dto.AuthorCreateDto;
 import ru.babushkina.libraryproject.dto.AuthorDto;
 import ru.babushkina.libraryproject.model.Author;
 
@@ -8,4 +9,5 @@ public interface AuthorService {
     AuthorDto getByNameV1(String name);
     AuthorDto getByNameV2(String name);
     AuthorDto getByNameV3(String name);
+    AuthorDto createAuthor(AuthorCreateDto authorCreateDto);
 }
