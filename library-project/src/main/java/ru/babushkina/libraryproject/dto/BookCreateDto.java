aa873e2bae78dfc7f790aa2d0af8b4f64c6d9be8
Bloody-Mary <marianelalieben@gmail.com>
2023-11-01 16:16:25 +0300

    feature:01.11.2023 16:16 private variables String name and Genre genre was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/dto/BookCreateDto.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/dto/BookCreateDto.java
@@ -1,4 +1,16 @@
 package ru.babushkina.libraryproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.babushkina.libraryproject.model.Genre;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class BookCreateDto {
+    private String name;
+    private Genre genre;
 }
