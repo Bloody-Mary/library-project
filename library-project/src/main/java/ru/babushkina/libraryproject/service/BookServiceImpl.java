0c45ac9e0f1a56f8e0ee93d4e2e9bcf3a2945f4f
Bloody-Mary <marianelalieben@gmail.com>
2023-11-01 16:33:58 +0300

    feature:01.11.2023 16:33 method createBook was created in class BookServiceImpl.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/BookServiceImpl.java
@@ -51,6 +51,8 @@ public class BookServiceImpl implements BookService{
 
     @Override
     public BookDto createBook(BookCreateDto bookCreateDto) {
-
+        Book book = bookRepository.save(convertDtoToEntity(bookCreateDto));
+        BookDto bookDto = convertSaveEntityToDto(book);
+        return bookDto;
     }
 }
