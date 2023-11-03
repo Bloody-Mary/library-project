10bbb9fa80b04c40314c0a3b448e0511b0b63444
Bloody-Mary <marianelalieben@gmail.com>
2023-11-03 15:52:58 +0300

    feature:03.11.2023 15:52 method updateAuthor was added.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/AuthorService.java
@@ -2,6 +2,7 @@ package ru.babushkina.libraryproject.service;
 
 import ru.babushkina.libraryproject.dto.AuthorCreateDto;
 import ru.babushkina.libraryproject.dto.AuthorDto;
+import ru.babushkina.libraryproject.dto.AuthorUpdateDto;
 import ru.babushkina.libraryproject.model.Author;
 
 public interface AuthorService {
@@ -10,4 +11,5 @@ public interface AuthorService {
     AuthorDto getByNameV2(String name);
     AuthorDto getByNameV3(String name);
     AuthorDto createAuthor(AuthorCreateDto authorCreateDto);
+    AuthorDto updateAuthor(AuthorUpdateDto authorUpdateDto);
 }
