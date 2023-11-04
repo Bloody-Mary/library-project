c9d1e1777302d7e93d7cc6e43753f26b81a8bf78
Bloody-Mary <marianelalieben@gmail.com>
2023-11-04 23:24:37 +0300

    feature:04.11.2023 23:24 method getRegisterPage was created.

--- simple-form-project/src/main/java/ru/babushkina/simpleformproject/controller/UsersController.java
+++ simple-form-project/src/main/java/ru/babushkina/simpleformproject/controller/UsersController.java
@@ -1,4 +1,12 @@
 package ru.babushkina.simpleformproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UsersController {
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register_page";
+    }
 }
