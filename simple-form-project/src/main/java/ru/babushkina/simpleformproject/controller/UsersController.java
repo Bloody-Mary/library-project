676bd3e3b56ef74076f5dce880ee220c3d298fb9
Bloody-Mary <marianelalieben@gmail.com>
2023-11-04 23:47:46 +0300

    feature:04.11.2023 23:47 method getLoginPage was created in class UsersController.

--- simple-form-project/src/main/java/ru/babushkina/simpleformproject/controller/UsersController.java
+++ simple-form-project/src/main/java/ru/babushkina/simpleformproject/controller/UsersController.java
@@ -9,4 +9,9 @@ public class UsersController {
     public String getRegisterPage() {
         return "register_page";
     }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login_page";
+    }
 }
