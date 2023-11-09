3d7262a2c3185481d45c7b72938b8a4ef084499f
Bloody-Mary <marianelalieben@gmail.com>
2023-11-09 19:39:59 +0300

    feature:09.11.2023 19:39 method getAuthorsView and private variable authorService were added.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/AuthorController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/AuthorController.java
@@ -2,8 +2,18 @@ package ru.babushkina.libraryproject.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.babushkina.libraryproject.service.AuthorService;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthorController {
+    private final AuthorService authorService;
+
+    @GetMapping("/authors")
+    String getAuthorsView(Model model) {
+        model.addAttribute("authors", authorService.getAllAuthors());
+        return "authors";
+    }
 }
