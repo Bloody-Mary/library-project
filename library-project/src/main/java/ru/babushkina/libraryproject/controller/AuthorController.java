d198cd9251a393af39399ce3cd8903c7abe954c8
Bloody-Mary <marianelalieben@gmail.com>
2023-11-09 19:36:58 +0300

    feature:09.11.2023 19:36 annotations @Controller and @RequiredArgsConstructor were added.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/AuthorController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/AuthorController.java
@@ -1,4 +1,9 @@
 package ru.babushkina.libraryproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class AuthorController {
 }
