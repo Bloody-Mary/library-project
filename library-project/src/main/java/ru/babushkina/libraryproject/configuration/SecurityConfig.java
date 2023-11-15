c290c94b5af6fb0047aa25aee0a6b7f27babcb26
Bloody-Mary <marianelalieben@gmail.com>
2023-11-15 17:11:11 +0300

    feature:15.11.2023 17:11 annotations @Configuration and @EnableWebSecurity were added.

--- library-project/src/main/java/ru/babushkina/libraryproject/configuration/SecurityConfig.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/configuration/SecurityConfig.java
@@ -1,4 +1,9 @@
 package ru.babushkina.libraryproject.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 }
