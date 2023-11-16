f77309892b040d8e806109de2e22a18af44f89be
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:37:39 +0300

    feature:16.11.2023 15:37 private Set with annotations @transient and @ManyToMany were added to class Role.

--- library-project/src/main/java/ru/babushkina/libraryproject/model/Role.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/Role.java
@@ -2,8 +2,12 @@ package ru.babushkina.libraryproject.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.Transient;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "t_role")
@@ -12,4 +16,8 @@ public class Role implements GrantedAuthority {
     @Id
     private Long id;
     private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
 }
