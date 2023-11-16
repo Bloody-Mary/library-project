203a0c463582dbd45aab7b14fb1a9cf710dc2dda
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:53:26 +0300

    feature:16.11.2023 15:53 private variable EntityManager em was created in class UserService.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -1,6 +1,10 @@
 package ru.babushkina.libraryproject.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class UserService implements UserDetailsService {
+    @PersistenceContext
+    private EntityManager em;
 }
