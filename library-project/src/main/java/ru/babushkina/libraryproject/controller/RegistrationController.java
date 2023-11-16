c0b4e1b69ab479b3a2c73ae1808d60bdc9723dd2
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:28:26 +0300

    feature:16.11.2023 16:28 variable userService and method registration were created in the class RegistrationController.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/RegistrationController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/RegistrationController.java
@@ -1,7 +1,20 @@
 package ru.babushkina.libraryproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.babushkina.libraryproject.model.User;
+import ru.babushkina.libraryproject.service.UserService;
 
 @Controller
 public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
+    }
 }
