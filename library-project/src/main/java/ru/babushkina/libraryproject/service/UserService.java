0597d6636a289560e6453c467c8d75c11cd83162
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:52:03 +0300

    feature:16.11.2023 15:51 class UserService was created in package UserService.

---
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -0,0 +1,6 @@
+package ru.babushkina.libraryproject.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class UserService implements UserDetailsService {
+}
