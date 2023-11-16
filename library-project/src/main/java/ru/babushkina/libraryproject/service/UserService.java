c580311adde1078f56bf344c60aab8572f7f7d3e
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:14:04 +0300

    feature:16.11.2023 16:14 method deleteUser was created in the method deleteUser.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -60,4 +60,12 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
         return true;
     }
+
+    public boolean deleteUser(Long userId) {
+        if (userRepository.findById(userId).isPresent()) {
+            userRepository.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
 }
