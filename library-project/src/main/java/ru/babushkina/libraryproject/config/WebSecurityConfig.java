6fa0cb0ee38037c8b1325b3dcf20c90868c6dbef
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 17:04:41 +0300

    feature:16.11.2023 17:04 variable userService with annotation @Autowired was added to class WebSecurityConfig.

--- library-project/src/main/java/ru/babushkina/libraryproject/config/WebSecurityConfig.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/config/WebSecurityConfig.java
@@ -1,10 +1,15 @@
 package ru.babushkina.libraryproject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import ru.babushkina.libraryproject.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfiguration {
+    @Autowired
+    UserService userService;
+
 }
