e3fd8ea61ecbb62166e15bbbe8b8f0645d22d512
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:49:20 +0300

    feature:16.11.2023 16:49 private variable userService with annotation @Autowired was created in class AdminController.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/AdminController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/AdminController.java
@@ -1,7 +1,11 @@
 package ru.babushkina.libraryproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ru.babushkina.libraryproject.service.UserService;
 
 @Controller
 public class AdminController {
+    @Autowired
+    private UserService userService;
 }
