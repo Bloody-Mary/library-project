624323359d1020cff262736ccef0f32edd03c0d4
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:03:11 +0300

    feature:16.11.2023 16:03 method loadUserByUsername(String username) was created in class UserService.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -3,8 +3,11 @@ package ru.babushkina.libraryproject.service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ru.babushkina.libraryproject.model.User;
 import ru.babushkina.libraryproject.repository.RoleRepository;
 import ru.babushkina.libraryproject.repository.UserRepository;
 
@@ -20,4 +23,14 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
 }
