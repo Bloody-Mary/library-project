d0e731a53c9a70f91e67af12fdb917b333b78729
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:35:10 +0300

    feature:16.11.2023 16:35 method addUser was created in class RegistrationController.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/RegistrationController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/RegistrationController.java
@@ -3,9 +3,14 @@ package ru.babushkina.libraryproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.babushkina.libraryproject.model.User;
 import ru.babushkina.libraryproject.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -16,5 +21,21 @@ public class RegistrationController {
     public String registration(Model model) {
         model.addAttribute("userForm", new User());
         return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addUser(@ModelAttribute("userForm") @Valid User userForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
+            model.addAttribute("passwordError", "Passwords don't match");
+            return "registration";
+        }
+        if (!userService.saveUser(userForm)) {
+            model.addAttribute("usernameError", "User with this username is already exists");
+            return "registration";
+        }
+        return "redirect:/";
     }
 }
