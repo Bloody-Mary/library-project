72a9e5eee7dfecb217f97305accce2fd93bd5112
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:55:57 +0300

    feature:16.11.2023 15:55 variables userRepository,roleRepository and bCryptPasswordEncoder with annotation @Autowired were added to class UserService.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -2,9 +2,22 @@ package ru.babushkina.libraryproject.service;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ru.babushkina.libraryproject.repository.RoleRepository;
+import ru.babushkina.libraryproject.repository.UserRepository;
 
 public class UserService implements UserDetailsService {
     @PersistenceContext
     private EntityManager em;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 }
