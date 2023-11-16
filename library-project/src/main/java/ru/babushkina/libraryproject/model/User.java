e6f8d8204c8a3182d5952c40be5325953f7ddbd5
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:18:07 +0300

    feature:16.11.2023 15:18 private String username was added with annotation @Size.

--- library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
@@ -3,11 +3,17 @@ package ru.babushkina.libraryproject.model;
 import jakarta.persistence.*;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.validation.constraints.Size;
+
 @Entity
 @Table(name = "t_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min=2, message = "Не менее 5 знаков")
+    private String username;
+
 
 }
