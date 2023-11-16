139ddf5a9a5e97d9f2afa6513ea6e34ac13785ba
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:52:20 +0300

    feature:16.11.2023 16:52 method userList was created in class AdminController.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/AdminController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/AdminController.java
@@ -2,10 +2,18 @@ package ru.babushkina.libraryproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.babushkina.libraryproject.service.UserService;
 
 @Controller
 public class AdminController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("admin")
+    public String userList(Model model) {
+        model.addAttribute("allUsers", userService.allUsers());
+        return "admin";
+    }
 }
