bb97cf5a2a88e975ddbf5c38837dd0240283e813
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:55:02 +0300

    feature:16.11.2023 16:55 method deleteUser was created in class AdminController.

--- library-project/src/main/java/ru/babushkina/libraryproject/controller/AdminController.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/controller/AdminController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.babushkina.libraryproject.service.UserService;
 
 @Controller
@@ -15,5 +17,15 @@ public class AdminController {
     public String userList(Model model) {
         model.addAttribute("allUsers", userService.allUsers());
         return "admin";
+    }
+
+    @PostMapping("/admin")
+    public String deleteUser(@RequestParam(required = true, defaultValue = "") Long userId,
+                             @RequestParam(required = true, defaultValue = "") String action,
+                             Model model) {
+        if (action.equals("delete")) {
+            userService.deleteUser(userId);
+        }
+        return "redirect:/admin";
     }
 }
