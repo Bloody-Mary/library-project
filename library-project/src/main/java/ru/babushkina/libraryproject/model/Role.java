da8da0dfc0b78cf2f95ef39e36914d3acefee941
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:34:45 +0300

    feature:16.11.2023 15:34 implementation from GrantedAuthority was added to class Role.

--- library-project/src/main/java/ru/babushkina/libraryproject/model/Role.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/Role.java
@@ -2,8 +2,9 @@ package ru.babushkina.libraryproject.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "t_role")
-public class Role {
+public class Role implements GrantedAuthority {
 }
