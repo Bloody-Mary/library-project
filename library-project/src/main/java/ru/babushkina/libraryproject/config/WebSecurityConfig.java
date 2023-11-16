758770bab3531bf2d478e7c88c3b6cb212fb2392
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 17:06:53 +0300

    feature:16.11.2023 17:06 bean BCryptPasswordEncoder was created in class WebSecurityConfig.

--- library-project/src/main/java/ru/babushkina/libraryproject/config/WebSecurityConfig.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package ru.babushkina.libraryproject.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.babushkina.libraryproject.service.UserService;
 
 @Configuration
@@ -12,4 +14,8 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
     @Autowired
     UserService userService;
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
