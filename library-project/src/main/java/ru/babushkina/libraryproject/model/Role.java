a7e577082313e12eb67c4ebdded3d234253edcef
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:36:14 +0300

    feature:16.11.2023 15:36 private variables Long id with annotation @Id and String name were added to class Role.

--- library-project/src/main/java/ru/babushkina/libraryproject/model/Role.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/Role.java
@@ -1,10 +1,15 @@
 package ru.babushkina.libraryproject.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "t_role")
 public class Role implements GrantedAuthority {
+
+    @Id
+    private Long id;
+    private String name;
 }
