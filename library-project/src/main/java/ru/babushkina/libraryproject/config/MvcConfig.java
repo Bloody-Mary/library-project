20ce928df4b99b903b7d2a5f5174f5649cddf410
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 16:19:49 +0300

    feature:16.11.2023 16:19 annotation @Configuration was added to class MvcConfig.

--- library-project/src/main/java/ru/babushkina/libraryproject/config/MvcConfig.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package ru.babushkina.libraryproject.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 }
