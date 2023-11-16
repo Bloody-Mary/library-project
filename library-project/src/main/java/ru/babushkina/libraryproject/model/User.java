89e3789193185b9e380641a2e471928c673d799a
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 14:00:18 +0300

    feature:16.11.2023 14:00 private variable Long id was added to class User with annotations @Id and @GeneratedValue(IDENTITY).

--- library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
@@ -1,11 +1,13 @@
 package ru.babushkina.libraryproject.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "t_user")
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
