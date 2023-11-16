b506aed58613a0d438255d860fa5e85304ade15b
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 15:23:42 +0300

    feature:16.11.2023 15:23 annotations @AllArgsConstructor, @NoArgsConstructor,@Builder,@Getter and @Setter were added to class User.

--- library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
@@ -1,11 +1,17 @@
 package ru.babushkina.libraryproject.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.security.core.Transient;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.Size;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 @Entity
 @Table(name = "t_user")
 public class User implements UserDetails {
