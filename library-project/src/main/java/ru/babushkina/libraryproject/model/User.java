9a68cf8f5424567207813e3d310abb653674df35
Bloody-Mary <marianelalieben@gmail.com>
2023-11-16 13:58:19 +0300

    feature:16.11.2023 13:58 implementation from UserDetais was added to class User.

--- library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/model/User.java
@@ -2,8 +2,10 @@ package ru.babushkina.libraryproject.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "t_user")
-public class User {
+public class User implements UserDetails {
+
 }
