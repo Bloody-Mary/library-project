5ba15d1eb6bee3fa1d12906e0d7ee014ca096090
Bloody-Mary <marianelalieben@gmail.com>
2023-11-17 19:13:18 +0300

    feature:17.11.2023 19:13 constructor was added to the class UserService.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -8,4 +8,9 @@ import ru.babushkina.libraryproject.repository.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
