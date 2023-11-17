b4de9724b83caf7ecadbf64066538d16b67f96c1
Bloody-Mary <marianelalieben@gmail.com>
2023-11-17 19:57:21 +0300

    feature:17.11.2023 19:57 private userRepository, constructor and method findByUsername were created in the class UserServiceImpl.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserServiceImpl.java
@@ -1,4 +1,21 @@
 package ru.babushkina.libraryproject.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.babushkina.libraryproject.model.User;
+import ru.babushkina.libraryproject.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
