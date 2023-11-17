10ad0fa3f3c3debae695fb410a1b8d08f3c4e78b
Bloody-Mary <marianelalieben@gmail.com>
2023-11-17 19:05:24 +0300

    feature:17.11.2023 19:05 interface UserRepository was created in the package repository.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/UserRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/UserRepository.java
@@ -1,12 +1,4 @@
 package ru.babushkina.libraryproject.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import ru.babushkina.libraryproject.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-    @Query(value = "SELECT nextval(pg_get_serial_sequence('t_user', 'id'))", nativeQuery = true)
-    Long getNextId();
+public interface UserRepository {
 }
