a54708a7cd7d0c2789bab17ee2fc2f0b94416cd4
Bloody-Mary <marianelalieben@gmail.com>
2023-11-17 19:07:18 +0300

    feature:17.11.2023 19:07 method findByUsername was created in the class UserRepository.

--- library-project/src/main/java/ru/babushkina/libraryproject/repository/UserRepository.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package ru.babushkina.libraryproject.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.babushkina.libraryproject.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
