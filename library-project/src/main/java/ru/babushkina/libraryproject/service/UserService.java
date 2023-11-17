b225ec1febe7f15818946d2dba3ad9094c9b1e68
Bloody-Mary <marianelalieben@gmail.com>
2023-11-17 19:12:04 +0300

    feature:17.11.2023 19:12 private Strings userRepository and passwordEncode were added to the class UserService.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/UserService.java
@@ -1,8 +1,11 @@
 package ru.babushkina.libraryproject.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.babushkina.libraryproject.repository.UserRepository;
 
 @Service
 public class UserService {
-
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 }
