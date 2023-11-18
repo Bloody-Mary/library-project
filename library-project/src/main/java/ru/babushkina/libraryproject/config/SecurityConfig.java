218c8d8326b9a5e5ea181c5752b49afd973932ae
Bloody-Mary <marianelalieben@gmail.com>
2023-11-18 21:18:13 +0300

    feature:17.11.2023 21:18 method UserDetailsService was used to authorise users.

--- library-project/src/main/java/ru/babushkina/libraryproject/config/SecurityConfig.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/config/SecurityConfig.java
@@ -2,45 +2,42 @@ package ru.babushkina.libraryproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.babushkina.libraryproject.repository.UserRepository;
+import ru.babushkina.libraryproject.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
         http
-                .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/book").hasRole("USER")
+                .authorizeRequests((authorize) ->
+                        authorize
+                                .requestMatchers("/book").hasRole("USER")
                                 .requestMatchers("/book/v2").hasRole("ADMIN")
                                 .requestMatchers("/books").hasRole("ADMIN")
                                 .anyRequest().authenticated()
-                ).httpBasic(withDefaults());
+                )
+                .httpBasic();
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService users() {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        UserDetails user = users
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails admin = users
-                .username("admin")
-                .password("password")
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new UserDetailsServiceImpl(userRepository);
     }
 }
