21c314bd52ced18701fee87d54138eb02416436e
Bloody-Mary <marianelalieben@gmail.com>
2023-11-24 00:05:01 +0300

    feature:24.11.2023 12:04 annotations @Size and @NotBlank were added to class AuthorCreateDto.

--- library-project/src/main/java/ru/babushkina/libraryproject/dto/AuthorCreateDto.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/dto/AuthorCreateDto.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class AuthorCreateDto {
+    @Size(min = 3, max = 10)
+    @NotBlank(message = "Необходимо указать имя")
     private String name;
+    @NotBlank(message = "Необходимо указать фамилию")
     private String surname;
 }
