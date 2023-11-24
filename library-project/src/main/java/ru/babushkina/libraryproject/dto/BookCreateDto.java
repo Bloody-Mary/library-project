ac777de2bb6647d01a2386817c923cec3e53c843
Bloody-Mary <marianelalieben@gmail.com>
2023-11-24 16:47:50 +0300

    feature:24.11.2023 16:47 import from javax was changed to jakarta in class BookCreateDto.

--- library-project/src/main/java/ru/babushkina/libraryproject/dto/BookCreateDto.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/dto/BookCreateDto.java
@@ -2,9 +2,8 @@ package ru.babushkina.libraryproject.dto;
 
 import lombok.*;
 
-import javax.validation.constraints.Size;
-import javax.validation.constraints.NotBlank;
-import java.util.Set;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,10 +13,7 @@ public class BookCreateDto {
     @Size(min = 1, max = 25)
     @NotBlank(message = "Необходимо указать название")
     private String name;
-    @NotBlank(message = "Необходимо указать жанр")
     private String genre;
-    @NotBlank(message = "Необходимо указать номер id жанра")
     private Long genreId;
-    @NotBlank(message = "Необходимо указать номер id автора")
     private Long authorId;
 }
