4e9501ce8718273c63bb31585f9aba5b80365787
Bloody-Mary <marianelalieben@gmail.com>
2023-11-24 18:39:38 +0300

    feature:24.11.2023 18:39 annotation @Slf4j was added to class GenreServiceImpl.

--- library-project/src/main/java/ru/babushkina/libraryproject/service/GenreServiceImpl.java
+++ library-project/src/main/java/ru/babushkina/libraryproject/service/GenreServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.babushkina.libraryproject.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.babushkina.libraryproject.dto.AuthorDto;
 import ru.babushkina.libraryproject.dto.BookDto;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GenreServiceImpl implements GenreService {
     private final GenreRepository genreRepository;
 
