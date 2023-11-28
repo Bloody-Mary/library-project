a6ebd5eef76efeaa0f142d6eb889f0557f3505fc
Bloody-Mary <marianelalieben@gmail.com>
2023-11-28 17:42:58 +0300

    feature:28.11.2023 17:42 methods testGetAuthorById() and testGetAuthorByIdNotFound() were created in AuthorServiceTest.

--- library-project/src/test/java/ru/babushkina/libraryproject/service/AuthorServiceTest.java
+++ library-project/src/test/java/ru/babushkina/libraryproject/service/AuthorServiceTest.java
@@ -1,7 +1,56 @@
 package ru.babushkina.libraryproject.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.babushkina.libraryproject.dto.AuthorDto;
+import ru.babushkina.libraryproject.model.Author;
+import ru.babushkina.libraryproject.model.Book;
+import ru.babushkina.libraryproject.repository.AuthorRepository;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class AuthorServiceTest {
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private AuthorServiceImpl authorService;
+
+    @Test
+    public void testGetAuthorById() {
+        Long id = 1L;
+        String name = "John";
+        String surname = "Doe";
+        Set<Book> books = new HashSet<>();
+        Author author = new Author(id, name, surname, books);
+
+
+        when(authorRepository.findById(id)).thenReturn(Optional.of(author));
+
+        AuthorDto authorDto = authorService.getAuthorById(id);
+
+        verify(authorRepository).findById(id);
+        Assertions.assertEquals(authorDto.getId(), author.getId());
+        Assertions.assertEquals(authorDto.getName(), author.getName());
+        Assertions.assertEquals(authorDto.getSurname(), author.getSurname());
+    }
+
+    @Test
+    public void testGetAuthorByIdNotFound() {
+        Long id = 1L;
+        when(authorRepository.findById(id)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(IllegalStateException.class, () -> authorService.getAuthorById(id));
+
+        verify(authorRepository).findById(id);
+    }
 }
