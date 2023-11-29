34540390cb7191a450795a28e66dc8d91fecfe85
Bloody-Mary <marianelalieben@gmail.com>
2023-11-29 14:58:04 +0300

    feature:29.11.2023 14:58 class AuthorRestControllerTest was created.

--- library-project/src/test/java/ru/babushkina/libraryproject/controller/AuthorRestControllerTest.java
+++ library-project/src/test/java/ru/babushkina/libraryproject/controller/AuthorRestControllerTest.java
@@ -1,4 +1,10 @@
 package ru.babushkina.libraryproject.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthorRestControllerTest {
+
 }
