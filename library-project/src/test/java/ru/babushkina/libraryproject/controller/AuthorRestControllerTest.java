1f68f38489a23ed890be7a77e0207a1df62e5211
Bloody-Mary <marianelalieben@gmail.com>
2023-11-29 14:59:00 +0300

    feature:29.11.2023 14:58 private MockMvc was added to class AuthorRestControllerTest.

--- library-project/src/test/java/ru/babushkina/libraryproject/controller/AuthorRestControllerTest.java
+++ library-project/src/test/java/ru/babushkina/libraryproject/controller/AuthorRestControllerTest.java
@@ -1,10 +1,14 @@
 package ru.babushkina.libraryproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 public class AuthorRestControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
 }
