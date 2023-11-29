a62fad1a723d8104687fe0a558d8812713a29752
Bloody-Mary <marianelalieben@gmail.com>
2023-11-29 15:15:47 +0300

    feature:29.11.2023 15:15 method testGetAuthorByName() was added to class AuthorRestControllerTest.

--- library-project/src/test/java/ru/babushkina/libraryproject/controller/AuthorRestControllerTest.java
+++ library-project/src/test/java/ru/babushkina/libraryproject/controller/AuthorRestControllerTest.java
@@ -1,6 +1,8 @@
 package ru.babushkina.libraryproject.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +10,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.babushkina.libraryproject.dto.AuthorDto;
+import ru.babushkina.libraryproject.service.AuthorService;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,6 +24,12 @@ public class AuthorRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    private AuthorService authorService;
+
+    @InjectMocks
+    private AuthorController authorController;
+
     @Test
     public void testGetAuthorById() throws Exception {
         Long authorId = 1L;
@@ -27,10 +38,24 @@ public class AuthorRestControllerTest {
         authorDto.setName("Александр");
         authorDto.setSurname("Пушкин");
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/author/{id}", authorId))
+        mockMvc.perform(get("/author/{id}", authorId))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(authorDto.getId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorDto.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorDto.getSurname()));
+    }
+
+    @Test
+    public void testGetAuthorByName() throws Exception {
+        String authorName = "Николай";
+        AuthorDto expectedAuthorDto = new AuthorDto();
+        expectedAuthorDto.setName(authorName);
+
+        when(authorService.getByNameV1(authorName)).thenReturn(expectedAuthorDto);
+
+        mockMvc.perform(get("/author")
+                        .param("name", authorName))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(authorName));
     }
 }
